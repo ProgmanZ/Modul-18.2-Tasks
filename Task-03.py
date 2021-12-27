e2fddfb31e1c60980a70e94df5ed94f0c71f714c
dumphill@gmail.com <Py956jch>
2021-12-27 11:56:38 +0300

    Task 01 - Готово. Task 02 - Готово. Task 03 - Готово. Task 04 - Готово.

--- Task-03.py
+++ Task-03.py
@@ -5,10 +5,11 @@ print('Программа ввода IP адреса.')
 ip_address = []
 count = 1
 while True:
-    group = int(input(f'Введите {count} группу цифр IP адреса'))
-    if 0 < group < 255 and count == 2 or 4:
+
+    group = int(input(f'Введите {count} группу цифр IP адреса: '))
+    if 0 <= group < 255 and (count == 2 or count == 3):
         ip_address.append(group)
-    elif 0 <= group < 255 and count != 1 or 4:
+    elif 0 < group < 255 and (count == 1 or count == 4):
         ip_address.append(group)
     elif count == 1 or 4:
         print(f'{group}группа цифр должна быть больше 0 и меньше 255.')
@@ -16,8 +17,12 @@ while True:
     else:
         print('Непредвиденное событие.')
     count += 1
-    if count == 4:
+    if count == 5:
         break
-print(ip_address)
 
-
+print("Вы ввели следующий IP адрес: {first}.{second}.{third}.{fourth}.".format(
+        first=ip_address[0],
+        second=ip_address[1],
+        third=ip_address[2],
+        fourth=ip_address[3]
+))
